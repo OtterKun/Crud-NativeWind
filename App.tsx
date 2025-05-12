75fc2d339d19936c8b393cd86ff2fb534ac4e9be
Vicente Benevenuto <vicentebsvanny@gmail.com>
2025-05-11 23:26:36 -0300

    Adicionando arquivos

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "./global.css"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text class="text-blue-600">Open up App.tsx to start working on your app!</Text>
+      <Text class="p-8">test</Text>
       <StatusBar style="auto" />
     </View>
   );
