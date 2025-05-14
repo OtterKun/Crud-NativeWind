e2cd8600f8a177cb201e74914b5820bd2ee305c2
Vicente Benevenuto <vicentebsvanny@gmail.com>
2025-05-13 23:48:05 -0300

    criado compo de card e aplicado um basico para tela principal

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { Linking } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
-import "./global.css"
+import "./global.css";
+import { Card } from "./components/card.tsx";
 
 export default function App() {
   return (
@@ -9,15 +10,9 @@ export default function App() {
       <Text className="text-left text-xl font-bold text-sky-600 pl-5">Sub to Gigi Murin!</Text>
       <Text className="text-purple-400 text-right"
       onPress={() => Linking.openURL('https://github.com/OtterKun')}>Creator.</Text>
+      <Card />
+      <Card />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/card.tsx
+++ components/card.tsx
@@ -1,8 +1,9 @@
-import "../global.css/"
-import {FC} from "react";
+import "../global.css"
+import { View } from "react-native";
 
-const Card: FC<IProps> = ({}) => {
-
+export function Card() {
+  return (
+    <View className="rounded-xl border-4 border-amber-300 bg-fuchsia-50 overflow-hidden gap-4 h-32">
+    </View>
+  );
 }
-
-export default Card;
